d93390cddb27fe26a53ab44f0f94315e4efed9c2
tuhina-das <114300168+tuhina-das@users.noreply.github.com>
2023-09-23 16:29:41 +0000

    Wrote up project, debugged, tested.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,39 @@
 import './App.css'
 
+//Create a React app from scratch
+//Show a single h1 that says "Good morning" if between midnight and 12pm
+//"Good Evening" if between 6pm and midnight
+
+//variables to store message and color
+let message;
+let color = {
+  color: ""
+}
+
+//series of if statements to set the text and color
+  const date = new Date();
+  const hour = date.getHours();
+
+  if (hour>=18 && hour <= 23){
+    message = "Good evening!";
+    color.color = "blue";
+  }
+  else if (hour >= 13 && hour <= 17){
+    message = "Good afternoon!";
+    color.color = "green";
+  }
+  else if (hour >= 0 && hour <= 12){
+    message = "Good morning!";
+    color.color = "red";
+  }
+
+
 export default function App() {
   return (
     <main>
-      React ⚛️ + Vite ⚡ + Replit 🌀
+      <h1 style={color}>
+        {message}
+      </h1>
     </main>
   )
 }
